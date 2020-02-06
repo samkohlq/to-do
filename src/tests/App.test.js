7f4a11606875763fa84849fc9fdb7abdf09f6aba
Samantha Koh <philip.koh@techs-iMac-3.local>
2020-02-06 15:17:57 +0800

    add unit test to check that App component contains AddTodo component

--- src/tests/App.test.js
+++ src/tests/App.test.js
@@ -1,6 +1,7 @@
 import React from "react";
 import { shallow } from "enzyme";
 import App from "../components/App";
+import AddTodo from "../containers/AddTodo";
 import Footer from "../components/Footer";
 
 it("App component renders without crashing", () => {
@@ -9,4 +10,11 @@ it("App component renders without crashing", () => {
 
 test("Footer component renders without crashing", () => {
   shallow(<Footer />);
+});
+
+it("App component contains AddTodo component", () => {
+  const wrapper = shallow(<App />);
+  const addToDo = <AddTodo />;
+  // expect(wrapper.contains(welcome)).toBe(true);
+  expect(wrapper.contains(addToDo)).toEqual(true);
 });
