e5fc623a49081694b658ecac95228eac120979a0
Samantha Koh <philip.koh@techs-iMac-3.local>
2020-02-13 16:28:39 +0800

    added async/await syntax to todoController.js

--- backend/controllers/todoController.js
+++ backend/controllers/todoController.js
@@ -1,29 +1,25 @@
 import { Todo } from "../db/models";
 
-/** creates todo based on todo name provided by user */
-export const createTodo = (req, res) => {
-  // stores todo provided by user in variable
+export const createTodo = async (req, res) => {
   const value = req.body.value;
-  return Todo.create({
+  await Todo.create({
     value: value,
     completed: false
-  }).then(() => {
-    res.send("added a todo");
   });
+  res.send("added a todo");
 };
 
-export const retrieveTodo = (req, res) => {
+export const retrieveTodo = async (req, res) => {
   // TODO(samkohlq): return todo that corresponds with id provided by user
   const id = req.body.id;
-  return Todo.findAll({}).then(() => {
-    res.send("attempted to retrieve todo of id " + id);
-  });
+  await Todo.findAll({});
+  res.send("attempted to retrieve todo of id " + id);
 };
 
 /** updates completed status of todo, based on todo id provided by user */
-export const updateTodo = (req, res) => {
+export const updateTodo = async (req, res) => {
   const id = req.body.id;
-  return Todo.update(
+  await Todo.update(
     {
       completed: true
     },
@@ -32,23 +28,17 @@ export const updateTodo = (req, res) => {
         id: id
       }
     }
-  ).then(() => {
-    res.send("marked todo of specific id to completed");
-  });
+  );
+  res.send("marked todo of specific id to completed");
 };
 
 /** deletes todo based on todo id provided by user */
-export const deleteTodo = (req, res) => {
+export const deleteTodo = async (req, res) => {
   const id = req.body.id;
-  return Todo.destroy({
+  await Todo.destroy({
     where: {
       id: id
     }
-  })
-    .then(() => {
-      res.send("deleted todo of id " + id);
-    })
-    .catch(e => {
-      console.log("error" + e);
-    });
+  });
+  res.send("deleted todo of id " + id);
 };
