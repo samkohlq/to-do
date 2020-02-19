c941396ec530e838f38772b9be09dac1b8155e87
Samantha Koh <philip.koh@techs-iMac-3.local>
2020-02-19 23:55:49 +0800

    create setVisibilityFilter.js in separate action creator file for clarity

--- frontend/src/actions/setVisibilityFilter.js
+++ frontend/src/actions/setVisibilityFilter.js
@@ -1,10 +1,3 @@
-let nextTodoId = 0;
-export const addTodo = value => ({
-  type: "ADD_TODO",
-  id: nextTodoId++,
-  value
-});
-
 export function setVisibilityFilter(filter) {
   return { type: "SET_VISIBILITY_FILTER", filter };
 }
