de13bd15f858cc18c1344d84c6e4e96feb753bb2
Samantha Koh <philip.koh@techs-iMac-3.local>
2020-02-20 18:18:51 +0800

    Updates

---
+++ precache-manifest.641c0461aecf5beb1ba9961c7ce8019f.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "726dd60d309c48ea97ad55571a5346c1",
+    "url": "/to-do/index.html"
+  },
+  {
+    "revision": "a58112f4f6d5cd0d5d42",
+    "url": "/to-do/static/js/2.a6188743.chunk.js"
+  },
+  {
+    "revision": "928d7b5eb39d16fe9a880722c974b51e",
+    "url": "/to-do/static/js/2.a6188743.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "33c818fc121021f923ec",
+    "url": "/to-do/static/js/main.f433c042.chunk.js"
+  },
+  {
+    "revision": "8b9f7010519e44a820b7",
+    "url": "/to-do/static/js/runtime-main.0ea6de14.js"
+  }
+]);
